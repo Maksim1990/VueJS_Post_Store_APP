17afa5a008f4ef86f6109c7242c5a592ab84430f
Your Name <you@example.com>
2018-05-27 15:36:23 +0300

    - Added ability to show posts from Imgur APIs - Added ability to use Vue-resource API requests - Added shop cart functionality - Added shopping cart reactive logic - Added search bar functionality - Added ability to show found results in header

--- public/script.js
+++ public/script.js
@@ -1,1 +1,65 @@
-console.log('It works.');
+const FIXED_PRICE = 9.99;
+
+new Vue({
+    el: "#app",
+    data: {
+        total: 0,
+        items: [],
+        cart: [],
+        search:'',
+        lastSearch:''
+    },
+    methods: {
+        onSubmit:function () {
+            this.$http
+                .get('/search/'.concat(this.search))
+                .then(
+                function (result) {
+                    this.items=result.data;
+                    this.lastSearch=this.search;
+                });
+        },
+        addItem: function (index) {
+            this.total += 9.99;
+            var item = this.items[index];
+            var found = false;
+            for (var i = 0; i < this.cart.length; i++) {
+                if (this.cart[i].id === item.id) {
+                    found = true;
+                    this.cart[i].qty++;
+                    break;
+                }
+            }
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: FIXED_PRICE
+                });
+            }
+
+        },
+        inc:function (item) {
+            item.qty++;
+            this.total+=FIXED_PRICE;
+        },
+        dec:function (item) {
+            item.qty--;
+            this.total-=FIXED_PRICE;
+            if( item.qty<=0){
+                for (var i = 0; i < this.cart.length; i++) {
+                    if (this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1);
+                    }
+                }
+            }
+        }
+    },
+    filters: {
+        currency: function (value) {
+
+            return '$'+value.toFixed(2);
+        }
+    }
+});
